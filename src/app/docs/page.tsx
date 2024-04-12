69851888de957ff98410b118fc022fd8cb9c6057
edsonalcala <edson_alcala@msn.com>
2024-04-11 20:00:36 -0600

    update styles

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -4,11 +4,11 @@ export default function DocsPage() {
     return (
         <div className={`py-10 flex items-center justify-center`}>
             <Image
-                src="/under-construction.png" // Ensure the image path is correct
+                src="/under-construction.png"
                 alt="Under Construction"
-                width={450} // Set your desired width
-                height={450} // Set your desired height
-                objectFit="contain" // Adjusts the size of the image within its container
+                width={450}
+                height={450}
+                objectFit="contain"
             />
         </div>
     )
